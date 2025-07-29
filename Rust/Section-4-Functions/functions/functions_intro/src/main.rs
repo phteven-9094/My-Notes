7a9cea443c486288282c100af03eb8b7bc565dba
Stephen Hughes <stephen.hughes@wgu.edu>
2025-07-29 13:48:08 -0600

    Completed Section 4

--- Rust/Section-4-Functions/functions/functions_intro/src/main.rs
+++ Rust/Section-4-Functions/functions/functions_intro/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+    open_store("Downtown");
+    open_store("Uptown");
+    bake_pizza(5, "pepperoni");
+    swim_in_profit();
+    let result = square(5);
+    println!("The square of 5 is {result}");
+    let result_implicit = square_implicit(6);
+    println!("The square of 6 is {result_implicit}");
+
+    let result: () = mystery();
+
+    let multiplier = 3;
+
+    let calculation: i32 = {
+        let value = 5 + 4;
+        value * multiplier
+    };
+
+    println!("{calculation}");
+}
+
+fn open_store(neighborhood: &str) {
+    println!("Opening my pizza store in {neighborhood}");
+}
+
+fn bake_pizza(quantity: i32, topping: &str) {
+    println!("Baking {quantity} {topping} pizzas");
+}
+
+fn swim_in_profit() {
+    println!("So much $$$, so little time");
+}
+
+// Explicit Return Values
+fn square(number: i32) -> i32 {
+    return number * number;
+}
+
+// Implicit Return Values
+fn square_implicit(number: i32) -> i32 {
+    number * number // No need for 'return' keyword
+}
+
+// Unit Type
+fn mystery() {
+    println!("Hello there");
 }
--- Rust/Section-4-Functions/section-project/functions-project/functions_project/src/main.rs
+++ Rust/Section-4-Functions/section-project/functions-project/functions_project/src/main.rs
@@ -1,3 +1,53 @@
+/*
+Define an apply_to_jobs function that accepts a
+'number' parameter (an i32) and a 'title' parameter
+(a string). It should print out the string:
+"I'm applying to {number} {title} jobs".
+
+Example:
+apply_to_jobs(35, "Rust Developer")
+-> "I'm applying to 35 Rust Developer jobs" √
+
+Define an is_even function that accepts a 'number'
+parameter (an i32). The function should return a true
+if the number is even and a false if the number is
+odd.
+Examples:
+is_even(8) -> true
+is_even(9) -> false √
+
+Define an alphabets function that accepts a 'text'
+parameter (an &str). The function should return a
+tuple of two Booleans. The first Boolean should check
+if the text contains the letter 'a'. The second
+Boolean should check if the text contains the letter
+'z'. You can use the 'contains' method to check if a
+string contains a specific character. See the documentation:
+https://doc.rust-lang.org/std/primitive.str.html#method.contains
+
+Examples:
+println!("{:?}", alphabets("aardvark")); -> (true, false)
+println!("{:?}", alphabets("zoology"));  -> (false, true)
+println!("{:?}", alphabets("zebra"));    -> (true, true)
+*/
+
 fn main() {
-    println!("Hello, world!");
+    apply_to_jobs(10, "Rust Dev");
+    is_even(8);
+    is_even(9);
+    println!("{:?}", alphabets("aardvark"));
 }
+
+fn apply_to_jobs(number: i32, title: &str) {
+    println!("I'm applying to {number} {title} jobs.");
+} // Task 1
+
+fn is_even(number: i32) -> bool {
+    number % 2 == 0
+} // Task 2
+
+fn alphabets(text: &str) -> (bool, bool) {
+    let contains_a = text.contains('a');
+    let contains_z = text.contains('z');
+    return (contains_a, contains_z);
+} // Task 3
